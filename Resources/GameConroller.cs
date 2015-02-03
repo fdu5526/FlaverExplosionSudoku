be8db25c16625bd566934c4f5ea7c74688486061
Dave Yan <fdu5526@live.com>
2015-02-02 23:18:47 -0500

    initial commit, set up isometric box and camera

---
+++ Resources/GameConroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameConroller : MonoBehaviour {
+
+	public Rect rect;
+
+	// Use this for initialization
+	void Start () {
+		rect = new Rect(0, 0, 10, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
