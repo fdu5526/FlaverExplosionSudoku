c2f1ab3a1f96a82992e26fca082503fc24a06adf
Rhiannon Malia <rmalia@andrew.cmu.edu>
2015-02-03 23:44:56 -0500

    Test

---
+++ Resources/Person.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Person : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public abstract void Activate ();
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
