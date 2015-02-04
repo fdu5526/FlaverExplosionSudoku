a1d331de8a7c23c2285a0f1c5b8f5b55f3e847a1
Rhiannon Malia <rmalia@andrew.cmu.edu>
2015-02-04 16:50:27 -0500

    Prefab person

---
+++ Resources/Normal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Normal : Person {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	override public void Activate (){
+		print ("HEllo world");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
