e91f970c8fd06debea960b35fcfdd84bbcc12b67
Dave Yan <fdu5526@live.com>
2015-02-04 19:08:57 -0500

    has board being made based off of an int matrix now

--- Resources/Level1.cs
+++ Resources/Level1.cs
@@ -3,29 +3,49 @@ using System.Collections;
 
 public class Level1 : MonoBehaviour {
 
-	int rowCount = 10;
-	int colCount = 6;
-	GameObject[][] board;
+	/**
+	 *
+	 -1: dead space, cannot place anything
+	 	0: empty square
+	 	1: normal person
+	 	2: blogger
+	 	3: grandma
+	 */
+	int[,] board = new int[,]
+	{
+		{0, 0, 1, 0, 0},
+		{0, 1, 0, 1, 0},
+		{0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 1},
+		{0, 0, 0, 1, 0},
+	};
 
-	float gridWidth = 3f;
+	int colCount, rowCount;
+	float gridWidth = 6f;
 
 	// Use this for initialization
 	void Start () 
 	{
 		
+		colCount = board.GetLength(0);
+		rowCount = board.GetLength(1);
 
+		print(colCount);
+		print(board.Length);
 
-		
-		board = new GameObject[rowCount][];
+		this.transform.localScale = new Vector3(colCount*gridWidth, 1, rowCount*gridWidth);
+
+		// place people there
 		for(int r = 0; r < rowCount; r++)
 		{
-			board[r] = new GameObject[colCount];
 			for(int c = 0; c < colCount; c++)
 			{
-				board[r][c] = (GameObject)MonoBehaviour.Instantiate(Resources.Load("Prefabs/Person"));
-				board[r][c].transform.position = new Vector3(gridWidth*r-(rowCount/2*gridWidth), 
-																										 1f, 
-																										 gridWidth*c-(colCount/2*gridWidth));
+				if(board[r,c] == 1)
+				{
+					GameObject g = (GameObject)MonoBehaviour.Instantiate(Resources.Load("Prefabs/Person"));
+					g.transform.position = new Vector3(gridWidth*r-(rowCount/2*gridWidth), 
+																						 1f, gridWidth*c-(colCount/2*gridWidth));
+				}
 			}
 		}
 
