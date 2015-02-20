4ea1b7d348ba0d1cd1461f0180d0bc4b5e8b95a9
Rhiannon Malia <rmalia@andrew.cmu.edu>
2015-02-19 19:15:41 -0500

    UI Selection

--- Resources/DropDown.cs
+++ Resources/DropDown.cs
@@ -12,6 +12,11 @@ public class DropDown : MonoBehaviour {
 	public Dictionary <GameObject, string> buttonsToName = new Dictionary<GameObject, string>();
 	public Dictionary <string, GameObject> nameToButtons = new Dictionary<string, GameObject> ();
 	public Dictionary<string, Text> nameToText = new Dictionary<string, Text>();
+	
+	// sprites
+	public Sprite grandmaLit, grandmaUnlit, bloggerLit, bloggerUnlit, bestFriendLit, bestFriendUnlit;
+	
+	
 	public string[] options;
 	public bool initialized = false;
 
@@ -38,7 +43,7 @@ public class DropDown : MonoBehaviour {
 	void Start () {
 		// make list
 		selection = -1;
-		//toggles = menuPanel.GetComponent<ToggleGroup>();
+		toggles = menuPanel.GetComponent<ToggleGroup>();
 		audios = GetComponents<AudioSource>(); 
 	}
 
@@ -46,6 +51,7 @@ public class DropDown : MonoBehaviour {
 		selection = -1;
 		for(int i = 0; i < options.Length; i++){
 			GameObject b = nameToButtons[options[i]];
+			toggles.UnregisterToggle(b.GetComponent<Toggle>());
 			Destroy(b);
 		}
 		nameToButtons = new Dictionary<string, GameObject> ();
@@ -56,17 +62,44 @@ public class DropDown : MonoBehaviour {
 
 	public void createButtons(){
 		for(int i = 0; i < options.Length; i++){
-			GameObject button = (GameObject) Instantiate(buttonPrefab);
+			GameObject button = (GameObject) Instantiate(buttonPrefab, new Vector3(0,0,0), Quaternion.identity);
 			Text t = button.GetComponentInChildren<Text>();
 			nameToText.Add(options[i], t);
 			int num = inventory[options[i]];
-			button.GetComponentInChildren<Text>().text = num.ToString();
+			t.text = num.ToString();
 			if(namesToType.ContainsKey(options[i])){
+				
 				int index = namesToType[options[i]];
-				button.GetComponent<Button>().onClick.AddListener(() => setSelection (index));
+				//button.GetComponent<Button>().onClick.AddListener(() => setSelection (index));
+				button.GetComponent<Toggle>().onValueChanged.AddListener((value) => setSelection (value, index));
+				button.GetComponent<Toggle>().isOn = false;
+				button.GetComponent<Toggle>().group = toggles;
+				toggles.RegisterToggle(button.GetComponent<Toggle>());
+				
+				Image unlit = button.transform.Find("Background/Top").GetComponent<Image>();
+				Image lit = button.transform.Find("Background/Checkmark").GetComponent<Image>();
+				
+				switch(index){
+					case 2: // blogger
+						unlit.sprite = bloggerUnlit;
+						lit.sprite = bloggerLit;
+						break;
+					case 3: // grandma
+						unlit.sprite = grandmaUnlit;
+						lit.sprite = grandmaLit;
+						break;
+					case 4: // bestfriend
+						unlit.sprite = bestFriendUnlit;
+						lit.sprite = bestFriendLit;
+						break;
+					default:
+						break;
+				}
+				
 				buttonsToName.Add (button, options[i]);
 				nameToButtons.Add (options[i], button);
-				button.transform.SetParent(menuPanel, false);
+				button.transform.SetParent(menuPanel.transform);
+				//button.transform.SetParent(menuPanel, false);
 			}
 		}
 	}
@@ -80,7 +113,7 @@ public class DropDown : MonoBehaviour {
 		if(selection != -1)
 		{
 			string name = typeToNames[selection];
-			topButton.GetComponentInChildren<Text>().text = name + " " + inventory[name];
+			//topButton.GetComponentInChildren<Text>().text = name + " " + inventory[name];
 			if(inventory[name] <= 0){
 				selection = -1;
 			}
@@ -89,12 +122,15 @@ public class DropDown : MonoBehaviour {
 
 	public void resetButtons(){
 		// reset text on buttons
+		selection = -1;
+		if(toggles != null)
+			toggles.SetAllTogglesOff();
 		for (int i = 0; i < options.Length; i++) {
 			if(nameToButtons.ContainsKey(options[i])){
 				Text button = nameToText[options[i]];
 				if(button != null){
 					int num = inventory[options[i]];
-					button.text = options[i] + ": " + num;
+					button.text = num.ToString();
 				}
 			}
 		}
@@ -114,13 +150,13 @@ public class DropDown : MonoBehaviour {
 
 	}
 
-	void setSelection(int s){
-
-		audios[0].Play();
-		selection = s;
-		string name = typeToNames[s];
-		//topButton.GetComponentInChildren<Text>().text = name + " " + inventory[name];
-
+	void setSelection(bool on, int s){
+		if(on){
+			audios[0].Play();
+			selection = s;
+			string name = typeToNames[s];
+			//topButton.GetComponentInChildren<Text>().text = name + " " + inventory[name];
+		}
 	}
 
 }
